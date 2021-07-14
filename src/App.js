dfbe91ab5d9c8d034973ff26658f8135d548b792
Bear <bear.ranasinghe@gmail.com>
2021-07-14 18:47:51 -0400

    method to generate random code is initialized

--- src/App.js
+++ src/App.js
@@ -5,10 +5,26 @@ import ColorPicker from './components/ColorPicker/ColorPicker';
 import GameTimer from './components/GameTimer/GameTimer';
 import NewGameButton from './components/NewGameButton/NewGameButton';
 
+const colors = ['#7CCCE5', '#FDE47F', '#E04644', '#B576AD'];
+
 class App extends Component {
+  constructor() {
+    // JS requires that super be called before accessing 'this'
+    super();
+    // this.state is an object that holds "state" in its properties
+    this.state = {
+      selColorIdx: 0,
+      guesses: [],
+      code: this.genCode()
+    };
+  }
+
+  genCode = () => new Array(4).fill().map(() => Math.floor(Math.random() * colors.length));
+  
   render() {
     return (
       <div className="App">
+        Selected color: {colors[this.state.selColorIdx]}
         <header className="App-header">React Mastermind</header>
         <div className="flex-h">
           <GameBoard />
