5fa48698b49fe65c827efb65bf4ea18c6fd95df3
Bear <bear.ranasinghe@gmail.com>
2021-07-14 20:32:00 -0400

    app renders buttons that pass state to components

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import GuessRow from '../GuessRow/GuessRow';
 
-const GameBoard = ({guesses, colors}) => (
-    <div>
-      {
-        guesses.map(
-          (guess, index) => <GuessRow  key={index} guess={guess} colors={colors} rowIndex={index}  currentGuess={index === guesses.length - 1} />
-        )
-      }
-    </div>
+const GameBoard = (props) => (
+  <div>
+    {props.guesses.map((guess, idx) =>
+      <GuessRow
+        guess={guess}
+        colors={props.colors}
+        rowIdx={idx}
+        currentGuess={idx === (props.guesses.length - 1)}
+        key={idx}
+      />
+    )}
+  </div>
 );
 
 export default GameBoard;
